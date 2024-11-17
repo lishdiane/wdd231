246dc87f1bda669e75ac185aedf9f451e675a362
lishdiane <meafunkymunky@yahoo.com>
2024-11-16 19:16:43 -0700

    initial commit

---
+++ chamber/scripts/script.js
@@ -0,0 +1,115 @@
+// ------ Year and modification date-----//
+
+const year = document.querySelector("#currentYear");
+const modified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+modified.innerHTML = document.lastModified;
+
+
+//-----hamburger menu-----//
+const button = document.querySelector(".menu");
+const nav = document.querySelector(".navigation");
+
+button.addEventListener("click", () => {
+    button.classList.toggle("show");
+    nav.classList.toggle("show");
+});
+
+//-----Create business cards-----//
+let results = null;
+
+getBusinesses();
+
+async function getBusinesses() {
+    const response = await fetch("data/members.json");
+    if (response.ok) {
+        const data = await response.json();
+        results = data;
+        createBusinessCards(results);
+    }
+}
+
+function cardTemplate(business) {
+    return `<section class="card">
+        <img loading="lazy" src="${business.image}" alt="${business.name} image" width="200" height="200">
+        <p>${business.name}</p>
+        <p>${business.address}</p>
+        <p>${business.phone_numbers}</p>
+        <p><a href="${business.url}">${business.url}</a></p>
+    </section>`
+}
+
+
+function createBusinessCards(array) {
+    const business = array.map((item) => cardTemplate(item))
+    document.querySelector(".business-cards").innerHTML = business.join("");
+}
+
+
+//-----List/grid view-----//
+
+const listButton = document.querySelector(".list-view");
+const gridButton = document.querySelector(".grid-view");
+const businessCards = document.querySelector(".business-cards");
+
+listButton.addEventListener("click", () => {
+    businessCards.classList.add("list");
+});
+
+gridButton.addEventListener("click", () => {
+    businessCards.classList.remove("list");
+});
+
+//-----Search-----//
+
+const searchButton = document.querySelector("#search-button");
+const input = document.querySelector("#search");
+const message = document.querySelector(".message");
+let filteredArray = [];
+
+input.addEventListener("keyup", () => {
+    let value = input.value;
+    filteredArray = [];
+    filterList(value);
+});
+
+function filterList(searchValue) {
+    const lowerValue = searchValue.toLowerCase();
+
+    if (lowerValue !== "") {
+        results.forEach(element => {
+            
+            let lowerName = element.name.toLowerCase();
+
+            if (lowerName.startsWith(lowerValue)) {
+                filteredArray = results.filter(item => {
+                    lowerName = item.name.toLowerCase();
+                    return lowerName.startsWith(lowerValue);
+                });
+            };
+
+            createBusinessCards(filteredArray);
+
+            if (filteredArray.length == 0) {
+                createBusinessCards(results);
+            };
+
+            searchButton.addEventListener("click", () => {
+                if (filteredArray.length == 0) {
+                    message.hidden = false;
+                    input.value = "";
+                } else {
+                    createBusinessCards(filteredArray);
+                };
+            });
+        });
+
+    } else {
+        createBusinessCards(results);
+    }
+};
+
+input.addEventListener("focus", () => message.hidden = true);
